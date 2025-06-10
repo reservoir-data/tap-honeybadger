1427b7dac2a24abb3a18a27c147dc06a7db6243c
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-06-10 14:51:09 -0600

    test: Ignore missing records for `teams` stream

--- tests/test_core.py
+++ tests/test_core.py
@@ -15,6 +15,9 @@ TestTapHoneybadger = get_tap_test_class(
     config=SAMPLE_CONFIG,
     suite_config=SuiteConfig(
         max_records_limit=10,
-        ignore_no_records_for_streams=["faults"],
+        ignore_no_records_for_streams=[
+            "faults",
+            "teams",  # Not available in the Developer plan
+        ],
     ),
 )
