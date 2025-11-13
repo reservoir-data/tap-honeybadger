9d67064898f5393fb71e9257f3b9bf5e37792ca2
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-11-12 18:23:29 -0600

    break: Require Python 3.12+ (#193)
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tap_honeybadger/tap.py
+++ tap_honeybadger/tap.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+from typing import override
+
 from singer_sdk import Stream, Tap
 from singer_sdk import typing as th
 
@@ -23,12 +25,8 @@ class TapHoneybadger(Tap):
         ),
     ).to_dict()
 
+    @override
     def discover_streams(self) -> list[Stream]:
-        """Return a list of discovered streams.
-
-        Returns:
-            A list of Honeybadger streams.
-        """
         return [
             streams.Accounts(tap=self),
             streams.Projects(tap=self),
